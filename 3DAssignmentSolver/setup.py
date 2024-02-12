768eed2001a83fbf90965f2e2d21d66fa0f5956b
Jet Chung <jetchung1@gmail.com>
2024-02-12 15:04:10 +0000

    Update setup.py

--- 3DAssignmentSolver/setup.py
+++ 3DAssignmentSolver/setup.py
@@ -3,12 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='AssignmentSolver',
     version='1.0',
-    py_modules=['main', 'utils', 'pulp_solver', 'gurobi_solver'],
+    py_modules=['main', 'utils'],
     packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jet-chung-1/3D-Assignment-Problem,
+    license='MIT',
     install_requires=[
         'numpy',
         'scipy',
-        'gurobipy',
-        'pulp'
+        'pandas',
+        'seaborn',
+        'matplotlib',
+        'timeit'
     ],
 )
